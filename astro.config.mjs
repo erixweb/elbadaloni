e220bb94d8e1c4c2e1d14de1f840413b741c030f
erixweb <erikmorenodev@gmail.com>
2023-09-16 12:01:02 +0200

    Add "Neteja de males herbes"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+import mdx from "@astrojs/mdx";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx(), svelte()],
   output: "server",
   adapter: netlify()
 });
---
+++ src/components/tweet.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let picture
+	export let displayName
+	export let username
+	export let date
+	export let tweetUrl
+</script>
+
+<article class="bg-slate-200 rounded-[12px] p-[12px] max-w-[580px] m-auto mt-[20px] mb-[20px]">
+	<a href={tweetUrl} rel="nofollow noindex" target="_blank" class="relative">
+        <div class="absolute top-0 right-0 bg-zinc-800 text-white font-semibold rounded-full px-[12px] py-[2px] hover:bg-zinc-600 duration-[250ms]">Veure en twitter</div>
+		<div class="flex gap-[20px]">
+			<img
+				src={picture}
+				alt={`Perfil de ${displayName}`}
+				class="rounded-full w-[45px] h-[45px] object-cover mt-0"
+			/>
+			<div>
+				<h3 class="font-semibold">
+					{displayName}
+				</h3>
+				<small class="text-gray-600">
+					@{username}
+				</small>
+				<p>
+					<slot />
+				</p>
+				<small class="text-gray-600">
+					{date}
+				</small>
+			</div>
+		</div>
+	</a>
+</article>
